d6d5a0c717e5234055eee19145fbcea47e0a0618
reyhaneh <r.sadatshokouhi@gmail.com>
2021-09-13 20:35:32 +0430

    share link modal

--- src/mobile/components/order/order.js
+++ src/mobile/components/order/order.js
@@ -25,6 +25,7 @@ import { history } from '../../../helpers/history'
 import { CancelProductOrder } from './cancelProductOrder'
 import { EditeProductOrder } from './editProductOrder'
 import { ShareLinkOrder } from "./shareLinkOrder"
+import { ShareLinkModal } from './shareLinkModal';
 
 
 
@@ -220,7 +221,7 @@ export const Order = ({ order, deliveryShow, setDeliveryShow, cancelOrderShow, s
                 </Row>
                 <Row className="p-0 m-0 pb-3 w-100">
                     <Col xs={12} className="p-0 px-1 pb-3">
-                        <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2" type="button" onClick={() => setIsShareLinkOrder(true)}>
+                        <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2" type="button" onClick={() => {setIsShareLinkOrder(true); setOrder(order.id);}}>
                             <Col xs={4} className="text-start align-items-center">
                                 <img  src={prevFactorIcon} height="20px" alt="prev-factor-icon" className="" />
                             </Col>
@@ -283,9 +284,11 @@ export const Order = ({ order, deliveryShow, setDeliveryShow, cancelOrderShow, s
             <CancelProductOrder show={cancelModalShow} onHide={() => { setCancelModalShow(false) }} productId={productId} orderId={orderId} />
             <EditeProductOrder show={editOrder} onHide={() => { setEditOrder(false) }} order={editProductOrder} />
             <AddNotesModal show={showNotesModal} onHide={() => { setShowNotesModal(false) }} permission={true} orderId={order.id} />
-            <Dialog onClose={() => setIsShareLinkOrder(false)} className={classes.backdrop} aria-labelledby="shareLink-dialog" open={isShareLinkOrder}>
+            {/* <Dialog onClose={() => setIsShareLinkOrder(false)} className={classes.backdrop} aria-labelledby="shareLink-dialog" open={isShareLinkOrder}>
                 <ShareLinkOrder orderId={isShareLinkOrder ? order.id : null} isShareLinkOrder={isShareLinkOrder} setIsShareLinkOrder={setIsShareLinkOrder} />
-            </Dialog>
+            </Dialog> */}
+            {console.log(order)}
+            <ShareLinkModal show={isShareLinkOrder} onHide={() => setIsShareLinkOrder(false)} orderId={order.id}/>
         </Card >
     )
 }
