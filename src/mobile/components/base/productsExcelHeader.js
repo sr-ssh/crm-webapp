2f86446a1cf31e6234ae6dd26c5c10de7c1b7eb6
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-09-18 12:18:17 +0430

    refactor style

--- src/mobile/components/base/productsExcelHeader.js
+++ src/mobile/components/base/productsExcelHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Col } from 'react-bootstrap';
 
 import backIcon from './../../assets/images/back.svg'
 import plusIcon from './../../assets/images/Products/pluss-dark-blue.svg'
@@ -14,14 +14,16 @@ export const Header = ({ title, getExcel, setModalShow, userPermission }) => {
             <Navbar variant="dark" sticky="top" className="py-2 my-nav noPrint" >
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="align-items-center w-100">
-                        <Nav.Link className="pe-3 p-0" onClick={() => setModalShow(true)}><img src={plusIcon} height="36px" alt="plus-icon" className="noPrint" /></Nav.Link>
-                        {(userType == 2 && !userPermission) ? null :
+                    <Nav className="align-items-center w-100 d-flex justify-content-between">
+                        <Col className="d-flex flex-grow-0 ">
+                            <Nav.Link className="pe-3 p-0 d-flex justify-content-center align-items-center " onClick={() => setModalShow(true)}><img src={plusIcon} height="36px" alt="plus-icon" className="noPrint" /></Nav.Link>
+                            {(userType == 2 && !userPermission) ? null :
 
-                            <Nav.Link className="pe-3 ms-3" onClick={() => getExcel()}><img src={excelIcon} height="28px" alt="plus-icon" className="noPrint" /></Nav.Link>
-                        }
-                        <Navbar.Text className="me-4 fs-6 fw-normal text-light noPrint">{title}</Navbar.Text>
-                        <Nav.Link href="/dashboard" className="me-auto ps-4"><img src={backIcon} height="28px" alt="back-icon" /></Nav.Link>
+                                <Nav.Link className="ps-0" onClick={() => getExcel()}><img src={excelIcon} height="28px" alt="plus-icon" className="noPrint" /></Nav.Link>
+                            }
+                        </Col>
+                        <Navbar.Text className="fs-6 fw-normal text-light noPrint">{title}</Navbar.Text>
+                        <Nav.Link href="/dashboard" className="ps-4"><img src={backIcon} height="28px" alt="back-icon" /></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
