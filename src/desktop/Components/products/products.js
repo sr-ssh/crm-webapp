134a9cb5983364a2307c6bf8d1fa2bde79324955
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-09-30 20:06:08 +0330

    added new Style Product Dekstop

--- src/desktop/Components/products/products.js
+++ src/desktop/Components/products/products.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Card, Row, Alert, Spinner, Col } from 'react-bootstrap';
+import { Container, Spinner, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux'
-import moment from 'jalali-moment';
-import persianJs from 'persianjs/persian.min';
+
 import { Button } from '@material-ui/core'
 
 // Actions
@@ -12,10 +11,8 @@ import { AddProduct } from './addProduct'
 import { EditProduct } from './editProduct'
 import { Header } from '../base/headerExcel'
 import { XlsxModal } from './xlsxModal';
+import { Product } from './product'
 
-
-// Icons
-import editIcon from '../../assets/images/Products/edit.svg'
 
 export const Products = () => {
 
@@ -44,7 +41,7 @@ export const Products = () => {
         <>
             <Header isBTNSearch={true} userPermission={userPermissions.excelProduct} isGetExcel={true} getExcel={() => setXlsxModalShow(true)} isBtnAdd={"اضافه محصول"} btnAdd={() => setAddModalShow(true)} />
             <div className="product-page d-flex flex-column align-items-center margin--top--header" style={{ paddingRight: sideBar.open ? "250px" : 0 }}>
-                <Container className="m-0 w-100 d-flex justify-content-center flex-wrap ">
+                <Container fluid className="m-0 px-4 w-100 d-flex justify-content-evenly flex-wrap ">
                     {
                         (productLoading) &&
                         <Col className="col-3 mt-2 m-auto d-block align-self-center w-100 mb-4 ">
@@ -53,32 +50,8 @@ export const Products = () => {
                     }
                     {products ?
                         (products.map((item, index) =>
-                            <Card key={index} className="m-auto mt-3 bg-light productCard col-3 mx-2" >
-                                <Card.Body className="pb-0 ps-1 rounded-3">
-                                    <Card.Title>
-                                        {item.active
-                                            ? <div className="activeStatus"><span></span> فعال</div>
-                                            : <div className="deActiveStatus"><span></span> غیرفعال</div>}
-                                    </Card.Title>
-                                    <Card.Text className="pt-1">
-                                        <span style={{ "color": "var(--text-color-one)" }}>نام : </span>{item.name && persianJs(item.name).englishNumber().toString()}
-                                    </Card.Text>
-                                    <Card.Text className="pt-1">
-                                        <span style={{ "color": "var(--text-color-one)" }}>قیمت فروش : </span>{item.sellingPrice && persianJs(item.sellingPrice).englishNumber().toString()}
-                                    </Card.Text>
-                                    <Card.Text className="pt-1">
-                                        <span style={{ "color": "var(--text-color-one)" }}>تاریخ ویرایش : </span>{item.updatedAt && persianJs(moment.from(item.updatedAt, 'YYYY/MM/DD').locale('fa').format('YYYY/MM/DD')).englishNumber().toString()}
-                                    </Card.Text>
-                                    <Card.Text className="pt-1 ps-1">
-                                        <span style={{ "color": "var(--text-color-one)" }}>توضیحات :   </span>{item.description && persianJs(item.description).englishNumber().toString()}
-                                    </Card.Text>
-                                    <Card.Link className="editLogo w-100 d-block m-auto" onClick={() => { setEditModalShow(true); setProduct(item) }}>
-                                        <img className="d-block me-auto" src={editIcon} height="42px" alt="back-icon" />
-                                    </Card.Link>
-                                </Card.Body>
-                            </Card>
+                            <Product key={index} sideBar={sideBar.open} item={item} setEditModalShow={() => setEditModalShow(true)} setProduct={(product) => setProduct(product)} />
                         ))
-
                         : null}
 
                     <AddProduct show={addModalShow} onHide={() => setAddModalShow(false)} />
