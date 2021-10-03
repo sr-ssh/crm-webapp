350075e6b7f5ed4a3f54bea9f6b6a0187c436139
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-10-03 10:33:21 +0330

    refactor Style

--- src/mobile/components/order/order.js
+++ src/mobile/components/order/order.js
@@ -386,12 +386,12 @@ export const Order = ({ order, deliveryShow, setDeliveryShow, cancelOrderShow, s
                             </Button>
                         </Col>
                     }
-                    <Col xs={6} className="p-0 px-1 pb-3 ps-2">
+                    {/* <Col xs={6} className="p-0 px-1 pb-3 ps-2">
                         <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2 pe-2" type="button" onClick={() => { setUploadModalShow(true); setActiveOrder(order) }}>
                             <img src={cancelIcon} height="25px" alt="print-icon" className="col-3" />
                             <span className="noPrint">بارگذاری مدارک</span>
                         </Button>
-                    </Col>
+                    </Col> */}
                 </Row>
             </Card.Body>
             <EditField show={editModalShow} onHide={() => { setEditModalShow(false); setInput(''); }} input={input} name={name} productId={productId} orderId={orderId} setInput={setInput} />
