8015aa49dc354ec7afe09c696479e59cbd453d15
reyhaneh <r.sadatshokouhi@gmail.com>
2021-10-04 10:31:47 +0330

    add result for sale opportunity in mobile

--- src/mobile/components/order/order.js
+++ src/mobile/components/order/order.js
@@ -29,6 +29,7 @@ import { CancelProductOrder } from './cancelProductOrder'
 import { EditeProductOrder } from './editProductOrder'
 import { ShareLinkModal } from './shareLinkModal';
 import { FinancialCheckModal } from './financialCheckModal'
+import { ResultOrder } from './resultOrder'
 
 
 
@@ -52,7 +53,7 @@ export const Order = ({ order, deliveryShow, setDeliveryShow, cancelOrderShow, s
     const [showNotesModal, setShowNotesModal] = useState(false)
     const [isShareLinkOrder, setIsShareLinkOrder] = useState(false)
     const [financialCheckModal, setFinancialCheckModal] = useState(false)
-
+    const [resultOrderModal, setResultOrderModal] = useState(false)
 
     const [input, setInput] = useState('')
     const [name, setName] = useState('')
@@ -393,27 +394,20 @@ export const Order = ({ order, deliveryShow, setDeliveryShow, cancelOrderShow, s
                             <span className="pe-1">پیک</span>
                         </Button>
                     </Col>
-                    {order.status === 3 &&
-                        <Col xs={6} className="p-0 px-1 pb-3 ps-2">
-                            <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2 pe-2" type="button" onClick={() => { setRecordOrderShow(true); setActiveOrder(order) }}>
-                                <img src={submitIcon} height="25px" alt="print-icon" className="col-3" />
-                                <span className="pe-1">ثبت</span>
-                            </Button>
-                        </Col>}
-                    {order.status === 3 &&
-                        <Col xs={6} className="p-0 px-1 pb-3 ps-2">
-                            <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2 pe-2" type="button" onClick={() => { setCancelOrderShow(true); setActiveOrder(order) }}>
-                                <img src={cancelIcon} height="25px" alt="print-icon" className="col-3" />
-                                <span className="pe-1">سفارش ناموفق</span>
-                            </Button>
-                        </Col>
-                    }
                     {/* <Col xs={6} className="p-0 px-1 pb-3 ps-2">
                         <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2 pe-2" type="button" onClick={() => { setUploadModalShow(true); setActiveOrder(order) }}>
                             <img src={cancelIcon} height="25px" alt="print-icon" className="col-3" />
                             <span className="noPrint">بارگذاری مدارک</span>
                         </Button>
                     </Col> */}
+                    {order.status === 3 &&
+                        <Col xs={6} className="p-0 px-1 pb-3 ps-2">
+                            <Button className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2 pe-2" type="button" onClick={() => { setResultOrderModal(true); setActiveOrder(order) }}>
+                                <img src={submitIcon} height="25px" alt="print-icon" className="col-3" />
+                                <span className="pe-1">نتیجه</span>
+                            </Button>
+                        </Col>
+                    }
                 </Row>
             </Card.Body>
             <EditField show={editModalShow} onHide={() => { setEditModalShow(false); setInput(''); }} input={input} name={name} productId={productId} orderId={orderId} setInput={setInput} />
@@ -422,6 +416,7 @@ export const Order = ({ order, deliveryShow, setDeliveryShow, cancelOrderShow, s
             <AddNotesModal show={showNotesModal} onHide={() => { setShowNotesModal(false) }} permission={true} orderId={order.id} />
             <ShareLinkModal show={isShareLinkOrder} onHide={() => setIsShareLinkOrder(false)} order={isShareLinkOrder ? shareLinkOrder : null} />
             <FinancialCheckModal show={financialCheckModal} onHide={() => setFinancialCheckModal(false)} order={financialCheckModal ? order : null} />
+            <ResultOrder show={resultOrderModal} onHide={() => setResultOrderModal(false)} order={resultOrderModal ? order : null} />
         </Card >
     )
 }
---
+++ src/mobile/components/order/resultOrder.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Modal, Row, Col, Form, Button, Spinner, Alert } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+
+// Actions
+import { orderActions } from '../../../actions'
+// Icons
+import closeIcon from '../../assets/images/close.svg'
+import cancelIcon from '../../assets/images/order/cancel.svg'
+import submitIcon from './../../assets/images/order/submit.svg'
+
+
+export const ResultOrder = (props) => {
+
+
+    const dispatch = useDispatch()
+    let failure = () => {
+        dispatch(orderActions.editOrderStatus(props.order.id, "4"))
+        setTimeout(() => {
+            dispatch(orderActions.getOrders({ status: 3 }))
+            props.onHide(false)
+
+        }, 1500);
+
+    }
+
+    let successful = () => {
+        dispatch(orderActions.editOrderStatus(props.order.id, "0"))
+        setTimeout(() => {
+            dispatch(orderActions.getOrders({ status: 3 }))
+            props.onHide(false)
+
+        }, 1500);
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            className="mx-3 order-serach-modal"
+        >
+            <Modal.Body className="add-product px-4 my-2">
+                <Button className="border-0 customer-modal-close--desktop" type="button" onClick={e => props.onHide(false)}>
+                    <img className="d-flex m-auto customer-modal-close-svg--desktop" src={closeIcon} alt="close-btn" />
+                </Button>
+                <Row className="d-flex flex-column justify-content-center align-items-center">
+                    <Col className="d-flex justify-content-center">
+                        <Button className="fw-bold order-submit border-0 w-75 mt-4 text-light fs-6 bg-success d-flex align-items-center " onClick={e => { successful() }} size="lg" block>
+                            <img src={submitIcon} height="30px" alt="edit-order-icon" className="mx-2" />
+                            <span>   موفق بود</span>
+                        </Button>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <Button className="fw-bold order-submit border-0 bg-danger text-light w-75 mt-4 fs-6 bg-danger d-flex align-items-center" size="lg"
+                            onClick={e => { failure() }}
+                            type="submit" block>
+                            <img src={cancelIcon} height="28px" alt="edit-order-icon" className="mx-2" />
+                            <span>نا موفق بود</span>
+                        </Button>
+
+
+                    </Col>
+
+                </Row>
+            </Modal.Body>
+        </Modal >
+    )
+}
