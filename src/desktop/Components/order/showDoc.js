e3ba4c9f273582d9423cbc74474cfe969fb487ab
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-10-06 17:31:58 +0330

    added new Style Show Doc order

--- src/desktop/Components/order/showDoc.js
+++ src/desktop/Components/order/showDoc.js
@@ -10,7 +10,7 @@ import { orderActions } from '../../../actions';
 
 // Icons
 import closeIcon from '../../assets/images/close.svg'
-import fileIcon from '../../assets/images/order/file.svg'
+import fileIcon from '../../assets/images/order/file-blue.svg'
 
 
 export const ShowDocuments = (props) => {
@@ -30,7 +30,7 @@ export const ShowDocuments = (props) => {
 
     useEffect(() => {
         if (props.show == true) {
-            dispatch(orderActions.showDoc({ orderId: props.order }))
+            dispatch(orderActions.showDoc({ orderId: props.order.id }))
             setDocs(documents)
         }
     }, [props.order, props.show])
@@ -40,32 +40,46 @@ export const ShowDocuments = (props) => {
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered
+            backdrop="static"
             className="mx-3 order-serach-modal--large"
         >
-            <Modal.Body className="add-product px-4" style={{ height: documents?.length == 0 ? null : "80vh" }}>
+            <Modal.Body className="add-product px-4" style={{ maxHeight: documents?.length == 0 ? null : "80vh" }}>
                 <Button className="border-0 customer-modal-close--desktop" type="button" onClick={e => { props.onHide(false); setProductNameValidated(false); }}>
                     <img className="d-flex m-auto customer-modal-close-svg--desktop" src={closeIcon} alt="close-btn" />
                 </Button>
-                <Card className="m-auto mt-2 mb-2 bg-light productCard lh-lg " style={{ height: "100%" }} >
+                <Card className="m-auto mt-2 mb-2 bg-light productCard lh-lg " style={{ maxHeight: "70vh" }} >
                     <Card.Body className={`card--body--show--doc--mobile p-3 px-4 mx-2 rounded-3 fs-6-sm ${documentsLoading && 'd-flex justify-content-center'} `}>
-                        {props.show && !documentsLoading && documents?.length == 0 && <><h6 className="mt-5 text-center">هنوز مدرکی برای این سفارش اضافه نشده است</h6><br /> <h6 className="mb-5 text-center text--dark--blue" style={{ cursor: "pointer" }} onClick={() => { props.onHide(false); setProductNameValidated(false); props.UploadModalShow() }}>بارگذاری مدرک</h6>  </>}
+                        {props.show && !documentsLoading && documents?.length == 0 && <><h6 className="mt-5 text-center">هنوز مدرکی برای این سفارش اضافه نشده است</h6><br /> <h6 className="mb-5 text-center text--dark--blue" style={{ cursor: "pointer" }} onClick={() => { props.onHide(false); setProductNameValidated(false); props.setActiveOrder(props.order); props.UploadModalShow() }}>بارگذاری مدرک</h6>  </>}
                         {documentsLoading ? <CircularProgress className="my-5 text-center" /> : null}
 
 
-                        <Row style={{ height: "100%" }}>
+                        <Row >
                             {
                                 props.show ?
-                                    documents?.length > 0 && documents?.map((doc, index) =>
-                                        <Col key={index} xs={4} className="mb-3 px-4">
+                                    documents?.length > 0 && documents?.map((doc, index) => {
+                                        const filetypes = /apng|avif|gif|jpg|jpeg|jfif|pjpeg|pjp|png|svg|webp/;
+                                        const extname = filetypes.test(doc.fileType);
+                                        console.log(extname)
+                                        return (<Col key={index} xs={4} className="pb-3 px-4">
                                             <Col>{doc.name}</Col>
                                             <Col>
                                                 <Card className="productCard bg-light lh-lg border--blue" style={{ 'height': '106px' }}>
-                                                    <Card.Body className="rounded-3">
-                                                        {doc.location && <img src={doc.location} alt="document-picc" className="doc--img img-fluid" />}
+                                                    <Card.Body className={`rounded-3 ${!extname ? 'd-flex flex-column justify-content-center align-items-center' : null}  `}>
+                                                        {
+                                                            extname ?
+                                                                doc.location && <img src={doc.location} alt="document-picc" className="doc--img img-fluid" />
+                                                                :
+                                                                <>
+                                                                    <img src={fileIcon} alt="document-pic" className="w-100" height="35px" />
+                                                                    <span className="fw-bold mt-1">{doc.fileType}</span>
+                                                                </>
+                                                        }
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
                                         </Col>
+                                        )
+                                    }
                                     )
                                     : null
                             }
