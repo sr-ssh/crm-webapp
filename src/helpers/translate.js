3d27becd87c6f252aa4563b06b11f188c19cfe38
reyhaneh <r.sadatshokouhi@gmail.com>
2021-10-28 11:41:34 +0330

    add getLead permission to employees

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -1,40 +1,22 @@
 export function translate(key) {
-    if (key === "addOrder")
-        return "ثبت سفارش"
-    else if (key === "getOrders")
-        return "سفارش ها"
-    else if (key === "reminder")
-        return "یادآوری"
-    else if (key === "getProducts")
-        return "محصول ها"
-    else if (key === "finance")
-        return "مالی"
-    else if (key === "getCustomers")
-        return "مشتریان"
-    else if (key === "getEmployees")
-        return "کارمندان"
-    else if (key === "getDiscounts")
-        return "تخفیف ها"
-    else if (key === "saleOpprotunity")
-        return "فرصت فروش"
-    else if (key === "family")
-        return "نام"
-    else if (key === "company")
-        return "نام شرکت"
-    else if (key === "address")
-        return "آدرس"
-    else if (key === "price")
-        return "قیمت"
-    else if (key === "quantity")
-        return "تعداد"
-    else if (key === "excelProduct")
-        return "اکسل محصول ها"
-    else if (key === "excelCustomer")
-        return "اکسل مشتریان"
-    else if (key === "financialCode")
-        return "کد اقتصادی و فکس"
-    else if (key === "nationalIDCode")
-        return "کدملی"
-    else if (key === "getAllSaleOpprotunity")
-        return "مشاهده تمام فرصت فروش ها"
+  if (key === "addOrder") return "ثبت سفارش";
+  else if (key === "getOrders") return "سفارش ها";
+  else if (key === "reminder") return "یادآوری";
+  else if (key === "getProducts") return "محصول ها";
+  else if (key === "finance") return "مالی";
+  else if (key === "getCustomers") return "مشتریان";
+  else if (key === "getEmployees") return "کارمندان";
+  else if (key === "getDiscounts") return "تخفیف ها";
+  else if (key === "saleOpprotunity") return "فرصت فروش";
+  else if (key === "family") return "نام";
+  else if (key === "company") return "نام شرکت";
+  else if (key === "address") return "آدرس";
+  else if (key === "price") return "قیمت";
+  else if (key === "quantity") return "تعداد";
+  else if (key === "excelProduct") return "اکسل محصول ها";
+  else if (key === "excelCustomer") return "اکسل مشتریان";
+  else if (key === "financialCode") return "کد اقتصادی و فکس";
+  else if (key === "nationalIDCode") return "کدملی";
+  else if (key === "getAllSaleOpprotunity") return "مشاهده تمام فرصت فروش ها";
+  else if (key === "getLead") return "سرنخ";
 }
