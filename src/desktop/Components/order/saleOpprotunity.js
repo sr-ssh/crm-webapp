787fe519939510ef6bfa28093d9becbb13babc91
reyhaneh <r.sadatshokouhi@gmail.com>
2021-10-30 10:57:48 +0330

    sort salesopportunities

--- src/desktop/Components/order/saleOpprotunity.js
+++ src/desktop/Components/order/saleOpprotunity.js
@@ -1,118 +1,160 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Container,
-  Card,
-  Table,
-  Row,
-  Col,
-  Spinner,
-  Button,
-} from "react-bootstrap";
-
-//icons
-import deliveryIcon from "./../../assets/images/order/delivery1.svg";
-import printIcon from "./../../assets/images/order/print.svg";
-import submitIcon from "./../../assets/images/order/submit.svg";
-import editIcon from "../../assets/images/Products/edit.svg";
-import deleteIcon from "../../assets/images/delete.svg";
-import editeOrderIcon from "../../assets/images/order/edit-order-list.svg";
-// import addNoteIcon from '../../assets/images/order/add-note-white.svg'
-import noteListIcon from "../../assets/images/order/note-list-white.svg";
-import SearchIcon from "@material-ui/icons/Search";
+import { Container, Row, Col } from "react-bootstrap";
 
 // Actions
 
 import { orderActions } from "../../../actions";
 
 //components
-import { Order } from './order';
-import { Delivery } from './delivery'
-import { RecordOrder } from './recordOrder'
-import { OrderSearch } from './search'
-import { Header } from '../base/header'
-import { CancelOrder } from './cancelOrder'
-import { UploadDocuments } from './uploadDoc'
-import {FreeSaleOpportunity} from './freeSaleOpportunity'
-import { ShowDocuments } from './showDoc'
-
+import { Order } from "./order";
+import { Delivery } from "./delivery";
+import { RecordOrder } from "./recordOrder";
+import { OrderSearch } from "./search";
+import { Header } from "../base/header";
+import { CancelOrder } from "./cancelOrder";
+import { UploadDocuments } from "./uploadDoc";
+import { FreeSaleOpportunity } from "./freeSaleOpportunity";
+import { ShowDocuments } from "./showDoc";
 
 export const SaleOpprotunity = () => {
+  const [recordOrderShow, setRecordOrderShow] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  const [deliveryShow, setDeliveryShow] = useState(false);
+  const [activeOrder, setActiveOrder] = useState({});
+  const [order, setOrder] = useState("");
+  const [cancelOrderShow, setCancelOrderShow] = useState(false);
+  const [uploadModalShow, setUploadModalShow] = useState(false);
+  const [showDocModalShow, setShowDocModalShow] = useState(false);
 
+  const [customerInfoShow, setCustomerInfoShow] = useState(false);
+  const [refresh, setRefresh] = useState(false);
 
+  const dispatch = useDispatch();
 
-    const [recordOrderShow, setRecordOrderShow] = useState(false)
-    const [modalShow, setModalShow] = useState(false)
-    const [deliveryShow, setDeliveryShow] = useState(false)
-    const [activeOrder, setActiveOrder] = useState({})
-    const [order, setOrder] = useState('')
-    const [cancelOrderShow, setCancelOrderShow] = useState(false)
-    const [uploadModalShow, setUploadModalShow] = useState(false)
-    const [showDocModalShow, setShowDocModalShow] = useState(false)
+  const orders = useSelector((state) => state.getOrders.orders);
+  let orderLoading = useSelector((state) => state.getOrders.loading);
+  const sideBar = useSelector((state) => state.sideBar);
 
-    const [customerInfoShow, setCustomerInfoShow] = useState(false)
-    const [refresh, setRefresh] = useState(false)
+  useEffect(() => {
+    !recordOrderShow && dispatch(orderActions.getOrders({ status: 3 }));
+  }, [dispatch, recordOrderShow]);
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(orderActions.getOrders({ status: 3 }));
+  }, [refresh]);
 
+  return (
+    <>
+      <Header
+        isBTNSearch={true}
+        searchModalShow={() => setModalShow(true)}
+        isBTNRequest={false}
+      />
 
-    const orders = useSelector(state => state.getOrders.orders)
-    let orderLoading = useSelector(state => state.getOrders.loading)
-    const sideBar = useSelector(state => state.sideBar)
-
-
-    useEffect(() => {
-        !recordOrderShow && dispatch(orderActions.getOrders({ status: 3 }))
-    }, [dispatch, recordOrderShow])
-
-    useEffect(() => {
-        dispatch(orderActions.getOrders({ status: 3 }))
-    }, [refresh])
-
-    console.log(orders)
-
-    return (
-        <>
-            <Header isBTNSearch={true} searchModalShow={() => setModalShow(true)} isBTNRequest={false} />
-
-            <div className="product-page orders w-100 margin--top--header mb-5" style={{ paddingRight: sideBar.open ? "250px" : 0 }}>
-                <Container fluid className="m-0 mt-5 w-100 d-flex justify-content-center flex-wrap " >
-                    {/* {
+      <div
+        className="product-page orders w-100 margin--top--header mb-5"
+        style={{ paddingRight: sideBar.open ? "250px" : 0 }}
+      >
+        <Container
+          fluid
+          className="m-0 mt-5 w-100 d-flex justify-content-center flex-wrap "
+        >
+          {/* {
                         orderLoading &&
                         <Col className="col-3 mt-5 m-auto d-block align-self-center w-100 mb-4 ">
                             <Spinner className="m-auto d-block" animation="border" />
                         </Col>
                     } */}
-                    {
-                        (orders.length === 0 && !orderLoading) ? (
-                            <Row className="justify-content-center align-items-center no-result-filter">
-                                <Col className="col-8 text-center">
-                                    هیج نتیجه ای یافت نشد!
-                                </Col>
-                            </Row>
-                        ) : null
-                    }
+          {orders.length === 0 && !orderLoading ? (
+            <Row className="justify-content-center align-items-center no-result-filter">
+              <Col className="col-8 text-center">هیج نتیجه ای یافت نشد!</Col>
+            </Row>
+          ) : null}
 
+          {orders.length > 0
+            ? orders
+                .sort(
+                  (or1, or2) =>
+                    Number(
+                      or2.sellers.some((seller) => seller.active === true)
+                    ) -
+                    Number(or1.sellers.some((seller) => seller.active === true))
+                )
+                .map((order, index) => {
+                  if (
+                    order.sellers.some((seller) => seller.active === true) &&
+                    order.status == 3
+                  )
+                    return (
+                      <Order
+                        key={index}
+                        refresh={refresh}
+                        setRefresh={setRefresh}
+                        order={order}
+                        deliveryShow={deliveryShow}
+                        setDeliveryShow={setDeliveryShow}
+                        recordOrderShow={recordOrderShow}
+                        setRecordOrderShow={setRecordOrderShow}
+                        setActiveOrder={setActiveOrder}
+                        setOrder={setOrder}
+                        status={3}
+                        cancelOrderShow={cancelOrderShow}
+                        setCancelOrderShow={setCancelOrderShow}
+                        setUploadModalShow={setUploadModalShow}
+                        uploadModalShow={uploadModalShow}
+                        setCustomerInfoShow={setCustomerInfoShow}
+                        setShowDocModalShow={setShowDocModalShow}
+                      />
+                    );
+                  else
+                    return (
+                      <FreeSaleOpportunity
+                        order={order}
+                        refresh={refresh}
+                        setRefresh={setRefresh}
+                      />
+                    );
+                })
+            : null}
 
-
-                    {(orders.length > 0) ?
-                        (orders.map((order, index) => {
-                             if(order.sellers.some(seller => seller.active === true) && order.status == 3)
-                                  return <Order key={index} refresh={refresh} setRefresh={setRefresh}  order={order} deliveryShow={deliveryShow} setDeliveryShow={setDeliveryShow} recordOrderShow={recordOrderShow} setRecordOrderShow={setRecordOrderShow} setActiveOrder={setActiveOrder} setOrder={setOrder} status={3} cancelOrderShow={cancelOrderShow} setCancelOrderShow={setCancelOrderShow} setUploadModalShow={setUploadModalShow} uploadModalShow={uploadModalShow}  setCustomerInfoShow={setCustomerInfoShow} setShowDocModalShow={setShowDocModalShow} />
-                              else
-                                  return <FreeSaleOpportunity order={order}  refresh={refresh} setRefresh={setRefresh} />
-                        }))
-                        : null}
-
-                    <OrderSearch show={modalShow} onHide={() => setModalShow(false)} status={3} />
-                    <Delivery show={deliveryShow} onHide={() => setDeliveryShow(false)} order={order} />
-                    <RecordOrder show={recordOrderShow} onHide={() => setRecordOrderShow(false)} order={activeOrder} />
-                    <CancelOrder status="4" show={cancelOrderShow} onHide={() => setCancelOrderShow(false)} order={activeOrder} />
-                    <UploadDocuments show={uploadModalShow} onHide={() => setUploadModalShow(false)} order={activeOrder} />
-                    {activeOrder.id && <ShowDocuments show={showDocModalShow} onHide={() => setShowDocModalShow(false)} order={activeOrder} UploadModalShow={() => setUploadModalShow(true)} setActiveOrder={setActiveOrder} />}
-               
-                </Container>
-            </div >
-        </>
-    )
-}
+          <OrderSearch
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            status={3}
+          />
+          <Delivery
+            show={deliveryShow}
+            onHide={() => setDeliveryShow(false)}
+            order={order}
+          />
+          <RecordOrder
+            show={recordOrderShow}
+            onHide={() => setRecordOrderShow(false)}
+            order={activeOrder}
+          />
+          <CancelOrder
+            status="4"
+            show={cancelOrderShow}
+            onHide={() => setCancelOrderShow(false)}
+            order={activeOrder}
+          />
+          <UploadDocuments
+            show={uploadModalShow}
+            onHide={() => setUploadModalShow(false)}
+            order={activeOrder}
+          />
+          {activeOrder.id && (
+            <ShowDocuments
+              show={showDocModalShow}
+              onHide={() => setShowDocModalShow(false)}
+              order={activeOrder}
+              UploadModalShow={() => setUploadModalShow(true)}
+              setActiveOrder={setActiveOrder}
+            />
+          )}
+        </Container>
+      </div>
+    </>
+  );
+};
--- src/mobile/components/order/saleOpprotunity.js
+++ src/mobile/components/order/saleOpprotunity.js
@@ -1,114 +1,150 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Container, Row, Col, Spinner,Alert } from 'react-bootstrap';
-
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 
 // Actions
 
-import { orderActions } from '../../../actions';
-
+import { orderActions } from "../../../actions";
 
 //components
-import { Header } from '../base/header2';
-import { Order } from './order';
-import { Delivery } from './delivery'
-import { RecordOrder } from './recordOrder'
-import { UploadDocuments } from './uploadDoc'
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { CancelOrder } from './cancelOrder'
-import {FreeSaleOpportunity} from './freeSaleOpportunity'
-
-
+import { Header } from "../base/header2";
+import { Order } from "./order";
+import { Delivery } from "./delivery";
+import { RecordOrder } from "./recordOrder";
+import { UploadDocuments } from "./uploadDoc";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { CancelOrder } from "./cancelOrder";
+import { FreeSaleOpportunity } from "./freeSaleOpportunity";
 
 export const SaleOpprotunity = () => {
+  const [recordOrderShow, setRecordOrderShow] = useState(false);
+  const [deliveryShow, setDeliveryShow] = useState(false);
+  const [activeOrder, setActiveOrder] = useState({});
+  const [order, setOrder] = useState("");
+  const [cancelOrderShow, setCancelOrderShow] = useState(false);
+  const [uploadModalShow, setUploadModalShow] = useState(false);
+  const [customerInfoShow, setCustomerInfoShow] = useState(false);
+  const [refresh, setRefresh] = useState(false);
 
+  const dispatch = useDispatch();
 
+  const orders = useSelector((state) => state.getOrders.orders);
+  let alert = useSelector((state) => state.alert);
 
-    const [recordOrderShow, setRecordOrderShow] = useState(false)
-    const [deliveryShow, setDeliveryShow] = useState(false)
-    const [activeOrder, setActiveOrder] = useState({})
-    const [order, setOrder] = useState('')
-    const [cancelOrderShow, setCancelOrderShow] = useState(false)
-    const [uploadModalShow, setUploadModalShow] = useState(false)
-    const [customerInfoShow, setCustomerInfoShow] = useState(false)
-    const [refresh, setRefresh] = useState(false)
+  let orderLoading = useSelector((state) => state.getOrders.loading);
 
+  useEffect(() => {
+    !recordOrderShow && dispatch(orderActions.getOrders({ status: 3 }));
+  }, [dispatch, recordOrderShow]);
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(orderActions.getOrders({ status: 3 }));
+  }, [refresh]);
 
+  return (
+    <div className="product-page orders ">
+      <Header className="noPrint" title="فرصت فروش" backLink="/" />
+      {alert.message && (
+        <>
+          <div className="modal-backdrop show"></div>
+          <Row className="justify-content-center text-center ">
+            <Alert variant={alert.type}>{alert.message}</Alert>
+          </Row>
+        </>
+      )}
+      {orderLoading && (
+        <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "90vh", position: "fixed", top: "64px" }}
+        >
+          <Row className="d-flex justify-content-center">
+            <Col className="col-12 d-flex justify-content-center ">
+              <CircularProgress />
+            </Col>
+          </Row>
+        </Container>
+      )}
+      <Container
+        className={`m-auto  `}
+        style={{ height: "90vh", position: "fixed", top: "64px" }}
+      >
+        {orders.length === 0 && !orderLoading ? (
+          <Row className="justify-content-center align-items-center no-result-filter">
+            <Col className="col-8 text-center">هیج نتیجه ای یافت نشد!</Col>
+          </Row>
+        ) : null}
 
-    const orders = useSelector(state => state.getOrders.orders)
-    let alert = useSelector(state => state.alert)
+        <Row
+          className="m-0 p-0 w-100 h-100"
+          style={{ overflowX: "hidden", overflowY: "scroll" }}
+        >
+          {!orderLoading && orders.length > 0
+            ? orders
+                .sort(
+                  (or1, or2) =>
+                    Number(
+                      or2.sellers.some((seller) => seller.active === true)
+                    ) -
+                    Number(or1.sellers.some((seller) => seller.active === true))
+                )
+                .map((order, index) => {
+                  if (
+                    order.sellers.some((seller) => seller.active === true) &&
+                    order.status == 3
+                  )
+                    return (
+                      <Order
+                        key={index}
+                        refresh={refresh}
+                        setRefresh={setRefresh}
+                        order={order}
+                        deliveryShow={deliveryShow}
+                        setDeliveryShow={setDeliveryShow}
+                        recordOrderShow={recordOrderShow}
+                        setRecordOrderShow={setRecordOrderShow}
+                        setActiveOrder={setActiveOrder}
+                        setOrder={setOrder}
+                        cancelOrderShow={cancelOrderShow}
+                        setCancelOrderShow={setCancelOrderShow}
+                        setUploadModalShow={setUploadModalShow}
+                        setCustomerInfoShow={setCustomerInfoShow}
+                      />
+                    );
+                  else
+                    return (
+                      <FreeSaleOpportunity
+                        order={order}
+                        refresh={refresh}
+                        setRefresh={setRefresh}
+                      />
+                    );
+                })
+            : null}
+        </Row>
 
-    let orderLoading = useSelector(state => state.getOrders.loading)
-
-
-    useEffect(() => {
-        !recordOrderShow && dispatch(orderActions.getOrders({ status: 3 }))
-    }, [dispatch, recordOrderShow])
-
-    useEffect(() => {
-        dispatch(orderActions.getOrders({ status: 3 }))
-    }, [refresh])
-
-
-
-    return (
-        <div className="product-page orders ">
-            <Header className="noPrint" title="فرصت فروش" backLink="/" />
-            {
-                    alert.message &&
-                    <>
-                        <div className="modal-backdrop show"></div>
-                        <Row className="justify-content-center text-center ">
-                            <Alert variant={alert.type}>
-                                {alert.message}
-                            </Alert>
-                        </Row>
-                    </>
-                }
-                 {
-                    orderLoading &&
-                    <Container className="d-flex justify-content-center align-items-center"  style={{ height: "90vh", position: "fixed", top: "64px" }} >
-                        <Row className="d-flex justify-content-center">
-                            <Col className="col-12 d-flex justify-content-center ">
-                            <CircularProgress />
-                            </Col>
-                        </Row>
-                    </Container>
-
-                }
-            <Container className={`m-auto  `} style={{ height: "90vh", position: "fixed", top: "64px" }} >
-                
-               
-                {
-                    (orders.length === 0 && !orderLoading) ? (
-                        <Row className="justify-content-center align-items-center no-result-filter">
-                            <Col className="col-8 text-center">
-                                هیج نتیجه ای یافت نشد!
-                            </Col>
-                        </Row>
-                    ) : null
-                }
-                 
-                <Row className="m-0 p-0 w-100 h-100" style={{ overflowX : "hidden" , overflowY: "scroll" }}>
-                {( !orderLoading &&  orders.length > 0) ?
-                    (orders.map((order, index) =>{
-                        if(order.sellers.some(seller => seller.active === true) && order.status == 3)
-                            return  <Order key={index} refresh={refresh} setRefresh={setRefresh} order={order} deliveryShow={deliveryShow} setDeliveryShow={setDeliveryShow} recordOrderShow={recordOrderShow} setRecordOrderShow={setRecordOrderShow} setActiveOrder={setActiveOrder} setOrder={setOrder} cancelOrderShow={cancelOrderShow} setCancelOrderShow={setCancelOrderShow} setUploadModalShow={setUploadModalShow} setCustomerInfoShow={setCustomerInfoShow}  />
-                        else
-                            return <FreeSaleOpportunity order={order}  refresh={refresh} setRefresh={setRefresh} />
-                    }))
-                    : null}
-                </Row>
-                
-                {/* <OrderSearch show={modalShow} onHide={() => setModalShow(false)} /> */}
-                <Delivery show={deliveryShow} onHide={() => setDeliveryShow(false)} order={order} />
-                <RecordOrder show={recordOrderShow} onHide={() => setRecordOrderShow(false)} order={activeOrder} />
-                <CancelOrder status="4" show={cancelOrderShow} onHide={() => setCancelOrderShow(false)} order={activeOrder} />
-                <UploadDocuments show={uploadModalShow} onHide={() => setUploadModalShow(false)} order={activeOrder} />
-            </Container>
-        </div>
-    )
-}
+        {/* <OrderSearch show={modalShow} onHide={() => setModalShow(false)} /> */}
+        <Delivery
+          show={deliveryShow}
+          onHide={() => setDeliveryShow(false)}
+          order={order}
+        />
+        <RecordOrder
+          show={recordOrderShow}
+          onHide={() => setRecordOrderShow(false)}
+          order={activeOrder}
+        />
+        <CancelOrder
+          status="4"
+          show={cancelOrderShow}
+          onHide={() => setCancelOrderShow(false)}
+          order={activeOrder}
+        />
+        <UploadDocuments
+          show={uploadModalShow}
+          onHide={() => setUploadModalShow(false)}
+          order={activeOrder}
+        />
+      </Container>
+    </div>
+  );
+};
