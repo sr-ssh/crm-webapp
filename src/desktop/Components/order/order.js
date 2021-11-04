7f46a38fd1753a27cda94d2f78218eef4203b0ab
mjh99 <mohmadjavadheydari9@gmail.com>
2021-11-04 12:42:55 +0330

    fixed bug order Desktop. it wasn't showing btn set free sale opportunity on SaleOpportunity.

--- src/desktop/Components/order/order.js
+++ src/desktop/Components/order/order.js
@@ -156,7 +156,7 @@ export const Order = ({
       <Row className="m-0 mt-3 noPrint ">
         {order.sellers.some((seller) => seller.active === true) &&
           order.status == 3 &&
-          order.sellers[order.sellers.length - 1].id?._id === userInfo.data.id && (
+          order.sellers[order.sellers.length - 1].id?._id === userInfo.data._id && (
             <Col className="d-flex justify-content-end">
               <Button
                 className="w-100 btn-outline-dark btn--sale--opprotunity p-1 border-0 noPrint py-2 pe-2"
