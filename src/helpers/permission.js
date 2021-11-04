d19dd539ed02be624d5321bb5277d720b8d50ad9
mjh99 <mohmadjavadheydari9@gmail.com>
2021-11-04 13:36:31 +0330

    refactored helpers permission

--- src/helpers/permission.js
+++ src/helpers/permission.js
@@ -1,8 +1,11 @@
 export function isPermitted(route) {
   let permissions = JSON.parse(localStorage.getItem("permissions"));
+  let user_type = JSON.parse(localStorage.getItem('type'));
+
+
   if (route === "/products") {
     if (permissions?.getProducts) return true;
-  } else if (route === "/saleopprotunity") {
+  } else if (route === "/saleOpprotunity") {
     if (permissions?.saleOpprotunity) return true;
   } else if (route === "/orders") {
     if (permissions?.getOrders) return true;
@@ -11,42 +14,45 @@ export function isPermitted(route) {
   } else if (route === "/reminders") {
     if (permissions?.reminder) return true;
   } else if (route === "/discounts") {
-    if (permissions?.getDiscounts) return true;
+    if (permissions?.getDiscounts) return false;
   } else if (route === "/order/add") {
     if (permissions?.addOrder) return true;
-  } else if (
-    route === "/employees" ||
-    route === "/employee/add" ||
-    route === "/employees/add"
-  ) {
+  } else if (route === "/employees"){
     if (permissions?.getEmployees) return true;
-  } else if (
-    route === "/finance" ||
-    route === "/bills" ||
-    route === "/finance/bills"
-  ) {
+  } else if (route === "/employees/add"){
+    if (permissions?.employeeRequests) return true;
+  } else if (route === "/finance"){
     if (permissions?.finance) return true;
+  } else if(route === "/finance/bills"){
+    if (permissions?.currentCosts) return true;
   } else if (route === "/setting") {
-    return true;
+    if(user_type == "1") return true;
   } else if (route === "/lead") {
-    if (permissions?.getLead) return true;
-} else if (route === "/factor") {
-  if (permissions?.getReceipts) return true;
-} else if (route === "/factor/add") {
-  if (permissions?.addReceipt) return true;
-} else if (route === "/suppliers") {
-  if (permissions?.getSuppliers) return true;
-} else if (route === "/stock") {
-  if (permissions?.getStock) return true;
-  } else if (
-    [
-      "/",
-      "/register",
-      "/dashboard",
-      "/account",
-      "/factors",
-    ].includes(route)
-  )
+    if (permissions?.leads) return true;
+  } else if (route === "/factor") {
+    if (permissions?.getReceipts) return true;
+  } else if (route === "/factor/add") {
+    if (permissions?.addReceipt) return true;
+  } else if (route === "/factor/suppliers") {
+    if (permissions?.getSuppliers) return true;
+  } else if (route === "/factor/stock") {
+    if (permissions?.getStock) return true;
+  } else if (["/","/register","/dashboard","/account"].includes(route)){
     return true;
-  return false;
+  } else
+    return false;
+}
+
+
+
+export function getPermissionRoutesChilde(route) {
+  let isOK = false;
+  for(let a = 0 ; a < route.children.length ; a++ ){
+     let res = isPermitted(route.children[a].path)
+     if(res == true){
+      isOK = true;
+      break
+     } 
+  }
+  return isOK
 }
