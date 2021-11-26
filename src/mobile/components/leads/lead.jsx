4b9a632596e87f9a91608f6d07730eb28ab69ae7
mjh99 <mohmadjavadheydari9@gmail.com>
2021-11-26 17:34:53 +0330

    added addReminder on lead mobile

--- src/mobile/components/leads/lead.jsx
+++ src/mobile/components/leads/lead.jsx
@@ -1,80 +1,134 @@
-import React, { useEffect } from 'react'
-import { Card, Row, Button, Spinner, Col } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux'
-import persianJs from 'persianjs/persian.min';
+import React, { useEffect } from "react";
+import { Card, Row, Button, Spinner, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import persianJs from "persianjs/persian.min";
 // Actions
-import { leadActions } from '../../../actions'
+import { leadActions } from "../../../actions";
 
 // Icons
-import phoneIcon from './../../assets/images/lead/call.svg'
+import phoneIcon from "./../../assets/images/lead/call.svg";
+import reminderIcon from "./../../assets/images/reminder.svg";
 
-export const Lead = ({item, acceptLead, activeId, addOrder,failLead}) => {
+export const Lead = ({
+  item,
+  acceptLead,
+  activeId,
+  addOrder,
+  failLead,
+  setAddReminderModal,
+  ...props
+}) => {
+  const dispatch = useDispatch();
+  const editLoading = useSelector((state) => state.editLeadStatus.loading);
 
+  useEffect(() => {
+    activeId === item?._id && editLoading && dispatch(leadActions.getLeads());
+  }, [editLoading]);
 
-    const dispatch = useDispatch()
-    const editLoading = useSelector(state => state.editLeadStatus.loading)
+  return (
+    <>
+      <Card className={`m-auto mt-3 bg-light radius-10 border-0 mx-1`}>
+        <Card.Body className="pb-3 rounded-3">
+          <Card.Text className="pt-1">
+            <span className="text-gray fw-bold">نام : </span>
+            <span className="fs-6 me-2 fw-bold">
+              {item.family && persianJs(item.family).englishNumber().toString()}
+            </span>
+          </Card.Text>
+          {item?.accepted && (
+            <>
+              <Card.Text className="pt-1">
+                <Row>
+                  <Col xs={9}>
+                    <span className="text-gray fw-bold">شماره : </span>
+                    <span className="fs-6 me-2 fw-bold">
+                      {item.mobile &&
+                        persianJs(item.mobile).englishNumber().toString()}
+                    </span>
+                  </Col>
 
-    useEffect(() => {
-        (activeId === item?._id) && editLoading && dispatch(leadActions.getLeads())
-    }, [editLoading])
-
-
-    return (
-        <>
-        <Card className={`m-auto mt-3 bg-light radius-10 border-0 mx-1`} >
-        <Card.Body className="pb-3 ps-1 rounded-3">
-            <Card.Text className="pt-1">
-                <span className="text-gray fw-bold" >نام : </span>
-                <span className="fs-6 me-2 fw-bold">{item.family && persianJs(item.family).englishNumber().toString()}</span>
-            </Card.Text>
-            {
-                item?.accepted && <>
-                    <Card.Text className="pt-1">
-                    <Row>
-                    <Col xs={9}>
-                    <span className="text-gray fw-bold" >شماره : </span>
-                    <span className="fs-6 me-2 fw-bold">{item.mobile && persianJs(item.mobile).englishNumber().toString()}</span>
-                    </Col>
-
-                    
-                    <Col dir="ltr" className="top-16 ms-2 ">
-                        <Button onClick={(e) => addOrder(e, item?._id, item?.family, item?.mobile)} className="background--green button--green border-0 p-1">
-                            <img src={phoneIcon} alt="phone-icon" width="27px"/>
-                        </Button>
-                    </Col>
-                    </Row>
-                    </Card.Text>
-                    <Card.Text className="m-0 p-0 pt-1 d-flex align-items-start top-16">
-                        <span className="text-gray fw-bold text--description--product">توضیحات :</span>
-                        <span className="fs-6 me-2 fw-bold text--breake--dscription">{item.description && persianJs(item.description).englishNumber().toString()}</span>
-                    </Card.Text>
-                </>
-            }
-
-            <Card.Text className="m-0 p-0 pt-1 d-flex align-items-start ms-2">
-                {
-                    ((activeId === item?._id) && editLoading) ? (
-                        <Button className="button--green fs-6 fw-bold background--green border-0 w-50 mt-3" size="lg" type="submit" disabled>
-                            <Spinner
-                                as="span"
-                                animation="grow"
-                                size="sm"
-                                role="status"
-                                aria-hidden="true"
-                            />
-                            در حال انجام عملیات...
-                        </Button>
-                    ) : item?.accepted ?
-                        <Button onClick={(e) => failLead(e, item?._id)} className="button--red fs-6 fw-bold background--red border-0 w-50 mt-3" size="lg" type="submit" block>
-                            ناموفق
-                        </Button>
-                    :  <Button onClick={(e) => acceptLead(e, item?._id)} className="button--green fs-6 fw-bold background--green border-0 w-50 mt-3" size="lg" type="submit" block>
-                        قبول
+                  <Col dir="ltr" className="top-16 ms-2 ">
+                    <Button
+                      onClick={(e) =>
+                        addOrder(e, item?._id, item?.family, item?.mobile)
+                      }
+                      className="background--green button--green border-0 p-1"
+                    >
+                      <img src={phoneIcon} alt="phone-icon" width="27px" />
                     </Button>
-                }
-                </Card.Text>
+                  </Col>
+                </Row>
+              </Card.Text>
+              <Card.Text className="m-0 p-0 pt-1 d-flex align-items-start top-16">
+                <span className="text-gray fw-bold text--description--product">
+                  توضیحات :
+                </span>
+                <span className="fs-6 me-2 fw-bold ">
+                  {item.description &&
+                    persianJs(item.description).englishNumber().toString()}
+                </span>
+              </Card.Text>
+            </>
+          )}
+          {item?.accepted && (
+            <Card.Text className="d-flex justify-content-end ps-1 ">
+              <Button
+                className={`d-flex flex-row align-items-center justify-content-center btn--add--note--desktop--addOrder m-0 `}
+                style={{ width: "42%" }}
+                onClick={() => setAddReminderModal(item._id, item.family)}
+              >
+                <img
+                  className="me-3 p-1"
+                  src={reminderIcon}
+                  height="30px"
+                  alt="edit-icon"
+                />
+                <span className="me-1 fw-bold ms-3">یادآوری</span>
+              </Button>
+            </Card.Text>
+          )}
+
+          <Card.Text className="m-0 p-0 d-flex align-items-start w-100">
+            {activeId === item?._id && editLoading ? (
+              <Button
+                className="button--green fs-6 fw-bold background--green border-0 w-50 mt-3"
+                size="lg"
+                type="submit"
+                disabled
+              >
+                <Spinner
+                  as="span"
+                  animation="grow"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+                در حال انجام عملیات...
+              </Button>
+            ) : item?.accepted ? (
+              <Button
+                onClick={(e) => failLead(e, item?._id)}
+                className="button--red fs-6 fw-bold background--red border-0 w-100"
+                size="lg"
+                type="submit"
+                block
+              >
+                ناموفق
+              </Button>
+            ) : (
+              <Button
+                onClick={(e) => acceptLead(e, item?._id)}
+                className="button--green fs-6 fw-bold background--green border-0 w-100"
+                size="lg"
+                type="submit"
+                block
+              >
+                قبول
+              </Button>
+            )}
+          </Card.Text>
         </Card.Body>
-    </Card>
+      </Card>
     </>
-    )
-}
+  );
+};
--- src/mobile/components/leads/leads.js
+++ src/mobile/components/leads/leads.js
@@ -1,89 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Row, Spinner, Col } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux'
-import { history } from '../../../helpers';
+import React, { useEffect, useState } from "react";
+import { Container, Row, Spinner, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { history } from "../../../helpers";
 // Actions
-import { leadActions, employeeActions } from '../../../actions'
+import { leadActions, employeeActions } from "../../../actions";
 // Components
-import { Header } from '../base/productsExcelHeader2';
-import { AddLead } from './addLead'
-import { Lead } from './lead';
+import { Header } from "../base/productsExcelHeader2";
+import { AddLead } from "./addLead";
+import { Lead } from "./lead";
+import { AddReminder } from "../reminder/addReminder";
 
 export const Leads = () => {
+  const [addModalShow, setAddModalShow] = useState(false);
 
+  const [lead, setLead] = useState({});
+  const [activeId, setActiveId] = useState({});
 
-    const [addModalShow, setAddModalShow] = useState(false)
+  const dispatch = useDispatch();
+  const leads = useSelector((state) => state.getLeads.leads);
+  const loading = useSelector((state) => state.getLeads.loading);
+  const editLoading = useSelector((state) => state.editLeadStatus.loading);
+  const addloading = useSelector((state) => state.addLead.loading);
+  const uploadLoading = useSelector((state) => state.uploadExcel.loading);
+  const userPermissions = useSelector(
+    (state) => state.getPermissions.permissions
+  );
+  const [addReminderModal, setAddReminderModal] = useState(false);
 
-    const [lead, setLead] = useState({})
-    const [activeId, setActiveId] = useState({})
+  let uploadHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    const formData = new FormData();
+    formData.append("excel", e.target.files[0]);
+    dispatch(leadActions.uploadExcel(formData));
+  };
 
-    const dispatch = useDispatch()
-    const leads = useSelector(state => state.getLeads.leads)
-    const loading = useSelector(state => state.getLeads.loading)
-    const editLoading = useSelector(state => state.editLeadStatus.loading)
-    const addloading = useSelector(state => state.addLead.loading)
-    const uploadLoading = useSelector(state => state.uploadExcel.loading)
-    const userPermissions = useSelector(state => state.getPermissions.permissions)
-    
-    let uploadHandler = (e) => {
-        e.preventDefault()
-        console.log(e.target.files[0])
-        const formData = new FormData();
-        formData.append('excel', e.target.files[0])
-        dispatch(leadActions.uploadExcel(formData))
-    }
+  let acceptLead = (e, id) => {
+    e.preventDefault();
+    setActiveId(id);
+    dispatch(leadActions.editLeadStatus({ leadId: id, status: 0 }));
+  };
 
-    let acceptLead = (e, id) => {
-        e.preventDefault()
-        setActiveId(id)
-        dispatch(leadActions.editLeadStatus({leadId: id, status: 0}))
-    }
+  let failLead = (e, id) => {
+    e.preventDefault();
+    setActiveId(id);
+    dispatch(leadActions.editLeadStatus({ leadId: id, status: 1 }));
+  };
 
-    let failLead = (e, id) => {
-        e.preventDefault()
-        setActiveId(id)
-        dispatch(leadActions.editLeadStatus({leadId: id, status: 1}))
-    }
+  let addOrder = (e, id, family, mobile) => {
+    history.push({
+      pathname: "/order/add",
+      state: { id, family, mobile },
+    });
+  };
 
-    let addOrder = (e, id, family, mobile) => {
-        history.push({
-            pathname: '/order/add',
-            state: { id, family, mobile}
-        })
-    }
+  useEffect(() => {
+    if (!addModalShow) dispatch(leadActions.getLeads());
+  }, [dispatch, addModalShow, addloading, editLoading, uploadLoading]);
 
-    useEffect(() => {
-        if (!addModalShow)
-            dispatch(leadActions.getLeads())
-    }, [dispatch, addModalShow, addloading, editLoading, uploadLoading])
+  return (
+    <div className="product-page">
+      <Header
+        title="سرنخ"
+        uploadHandler={uploadHandler}
+        setModalShow={setAddModalShow}
+        userPermission={true}
+      />
+      <Container className="m-auto">
+        {editLoading === false && loading && (
+          <Row>
+            <Col className="col-3 mt-2 m-auto ">
+              <Spinner className="m-auto d-block" animation="border" />
+            </Col>
+          </Row>
+        )}
+        {leads
+          ? leads.map((item, index) => (
+              <Lead
+                key={index}
+                item={item}
+                acceptLead={acceptLead}
+                activeId={activeId}
+                addOrder={addOrder}
+                failLead={failLead}
+                setAddReminderModal={(e, title) => {
+                  setAddReminderModal(true);
+                  setActiveId({ idLead: e, title: title });
+                }}
+              />
+            ))
+          : null}
 
-
-    return (
-        <div className="product-page">
-            <Header title="سرنخ" uploadHandler={uploadHandler} setModalShow={setAddModalShow} userPermission={true} />
-            <Container className="m-auto">
-                {
-                    ((editLoading === false) && loading) &&
-                    <Row>
-                        <Col className="col-3 mt-2 m-auto ">
-                            <Spinner className="m-auto d-block" animation="border" />
-                        </Col>
-                    </Row>
-                }
-                {leads ?
-                    (leads.map((item, index) => <Lead 
-                                                key={index} 
-                                                item={item} 
-                                                acceptLead={acceptLead} 
-                                                activeId={activeId}
-                                                addOrder={addOrder} 
-                                                failLead={failLead}
-                                                />
-                    ))
-                    : null}
-
-                <AddLead show={addModalShow} onHide={() => setAddModalShow(false)} />
-            </Container>
-        </div>
-    )
-}
+        <AddLead show={addModalShow} onHide={() => setAddModalShow(false)} />
+        <AddReminder
+          show={addReminderModal}
+          onHide={() => setAddReminderModal(false)}
+          isPersonal={false}
+          aditional={{ typeReminder: 1, referenceId: activeId.idLead }}
+          title={activeId.title}
+        />
+      </Container>
+    </div>
+  );
+};
