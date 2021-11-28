d7a8919b8a4f3673676de659c5d0cd01d6b19763
reyhaneh <r.sadatshokouhi@gmail.com>
2021-11-28 14:04:43 +0330

    employee ui

--- src/desktop/Components/employee/editEmployee.js
+++ src/desktop/Components/employee/editEmployee.js
@@ -1,149 +1,179 @@
-import React, { useState, useEffect } from 'react'
-import { Modal, Button, Row, Col, Alert, Form, Spinner, Card } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import persianJs from 'persianjs/persian.min';
+import React, { useState, useEffect } from "react";
+import {
+  Modal,
+  Button,
+  Row,
+  Col,
+  Alert,
+  Form,
+  Spinner,
+  Card,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import persianJs from "persianjs/persian.min";
 // Actions
-import { employeeActions } from '../../../actions/employeeActions';
+import { employeeActions } from "../../../actions/employeeActions";
 // Helpers
-import { translate } from '../../../helpers';
+import { translate } from "../../../helpers";
 // Icons
-import closeIcon from '../../assets/images/close.svg'
+import closeIcon from "../../assets/images/close.svg";
 
 export const EditEmployee = (props) => {
+  const [validated, setValidated] = useState(false);
+  const dispatch = useDispatch();
+  let [newPermission, setNewPermission] = useState(props.employee.permission);
+  const [voipNo, setVoipNo] = useState({ voipNo: props.employee?.voipNumber });
+  let editEmployeeLoading = useSelector((state) => state.editEmployee.loading);
 
-    const [validated, setValidated] = useState(false)
-    const dispatch = useDispatch()
-    let [newPermission, setNewPermission] = useState(props.employee.permission)
-    const [voipNo , setVoipNo] = useState({voipNo : props.employee?.voipNumber})
-    let editEmployeeLoading = useSelector(state => state.editEmployee.loading)
+  const handleChange = (e) => {
+    let { name, type, value, checked } = e.target;
+    if (type == "checkbox") {
+      setNewPermission({ ...newPermission, [name]: checked });
+    } else setVoipNo({ voipNo: value });
+  };
 
-    const handleChange = (e) => {
-        let {  name, type, value, checked } = e.target
-        if (type == "checkbox") {
-            setNewPermission({ ...newPermission, [name]: checked })
-        }else setVoipNo({voipNo  : value })
+  const formHandler = (e) => {
+    e.preventDefault();
+    let employee = {
+      permissions: newPermission,
+      _id: props.employee._id,
+      voipNo: voipNo.voipNo,
+    };
+    dispatch(employeeActions.editEmployee(employee));
+    setTimeout(() => {
+      props.onHide(false);
+      setNewPermission(null);
+      dispatch(employeeActions.getEmployees());
+    }, 1500);
+  };
 
+  useEffect(() => {
+    if (props.show) {
+      setNewPermission(props.employee.permission);
+      setVoipNo({
+        voipNo: props.employee?.voipNumber ? props.employee?.voipNumber : null,
+      });
     }
+  }, [props.show]);
 
-    const formHandler = (e) => {
-        e.preventDefault()
-        let employee = { permissions: newPermission, _id: props.employee._id , voipNo: voipNo.voipNo }
-        dispatch(employeeActions.editEmployee(employee))
-        setTimeout(() => {
-            props.onHide(false)
-            setNewPermission(null) 
-            dispatch(employeeActions.getEmployees())
-        }, 1500);
-    }
-
-    useEffect(() => {
-        if (props.show){
-            setNewPermission(props.employee.permission)
-            setVoipNo({voipNo : props.employee?.voipNumber ? props.employee?.voipNumber : null })
-        }
-    }, [props.show])
-
-
-    return (
-        <Modal
-            {...props}
-            // size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            backdrop="static"
-            centered
-            className="mx-3 modal--edit--employee"
+  return (
+    <Modal
+      {...props}
+      // size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      backdrop="static"
+      centered
+      className="modal--edit--employee"
+    >
+      <Modal.Body
+        className="add-product px-4 permission-card applications-text-gray"
+        style={{ width: "800px" }}
+      >
+        <Button
+          className="border-0 customer-modal-close"
+          style={{ left: "-13px" }}
+          type="button"
+          onClick={(e) => {
+            props.onHide(false);
+            setNewPermission(null);
+          }}
         >
-            <Modal.Body className="add-product px-4 permission-card applications-text-gray" style={{ width : "400px"}} > 
-                <Button className="border-0 customer-modal-close" style={{ left: "-13px" }} type="button" onClick={e =>{ props.onHide(false); setNewPermission(null) } }>
-                    <img className="d-flex m-auto customer-modal-close-svg" src={closeIcon} alt="close-btn" />
-                </Button>
-                {/* {
-                    alert.message &&
-                    <>
-                        <div className="modal-backdrop show"></div>
-                        <Row className="justify-content-center text-center ">
-                            <Alert variant={alert.type}>
-                                {alert.message}
-                            </Alert>
-                        </Row>
-                    </>
-                } */}
-                <Form onSubmit={formHandler} noValidate validated={validated}>
-                    <Row className="m-0 p-0 ">
-                        <Col>
-                            نام :
-                            <span className="me-2">{props.employee.family}</span>
-                        </Col>
-                    </Row>
-                    <Row className="m-0 p-0 mt-3">
-                        <Col>
-                            موبایل:
-                            <span className="me-2" >{props.show && persianJs(props.employee.mobile).englishNumber().toString()}</span>
-                        </Col>
-                    </Row>
-                    <Row className="m-0 p-0 mt-3" >
-                            <Col className="p-0">
-                                <Card className="border-0 bg-transparent text-light">
-                                    <Form.Label className="pe-3 fs-6">sip</Form.Label>
-                                    <Form.Control className="order-input address-input"
-                                        type="number"
-                                        name="voipNumber"
-                                        defaultValue={props.employee?.voipNumber}
-                                        onChange={handleChange}
-                                    />
-                                </Card>
-                            </Col>
-                    </Row>
-                    <Card className="m-auto mt-3 productCard border-0 lh-lg pb-2" >
-                        <Card.Body className="pb-0 ps-0 emplyees-text-gray">
-                            <Row className="m-0 p-0 d-flex align-items-center " >
-                            <Col xs={6} className="p-0 ">
-                                <Card.Text className="employees--p--gray--desktop ">
-                                    سطح دسترسی:
-                                </Card.Text>
-                            </Col>
-                                    {
-                                        props.show && newPermission && Object.keys(newPermission).map((key, index) => {
-                                            if(key == "getDiscounts")return
-                                            return (
-                                                <Col xs={6} className="p-0 mt-2 employees-text-permission--desktop d-flex  align-items-center ">
-                                                    <label className="mb-0 p-0 option--time--filter--groupe">
-                                                        <input
-                                                            type="checkbox"
-                                                            name={key}
-                                                            id={`${index}`}
-                                                            onChange={handleChange}
-                                                            defaultChecked={newPermission[key]}
-                                                        />
-                                                        <span><p className="p-0 m-0 ">{translate(key)}</p></span>
-                                                    </label>
-                                                </Col>
-                                            )
-                                        })
-                                    }
-                            </Row>
-                        </Card.Body>
-                    </Card>
-                    {
-                        editEmployeeLoading ? (
-                            <Button className="fw-bold order-submit border-0 w-100 mt-3" size="lg" type="submit" disabled>
-                                <Spinner
-                                    as="span"
-                                    animation="grow"
-                                    size="sm"
-                                    role="status"
-                                    aria-hidden="true"
-                                />
-                                در حال انجام عملیات...
-                            </Button>
-                        ) : (
-                            <Button className="fw-bold btn-dark-blue notes-round border-0 w-100 mt-3" size="lg" type="submit" block>
-                            ثبت
-                        </Button>
-                        )
-                    }
-                </Form>
-            </Modal.Body>
-        </Modal>
-    )
-}
+          <img
+            className="d-flex m-auto customer-modal-close-svg"
+            src={closeIcon}
+            alt="close-btn"
+          />
+        </Button>
+        <Form onSubmit={formHandler} noValidate validated={validated}>
+          <Row className="m-0 p-0 ">
+            <Col>
+              نام :<span className="me-2">{props.employee.family}</span>
+            </Col>
+            <Col>
+              موبایل:
+              <span className="me-2">
+                {props.show &&
+                  persianJs(props.employee.mobile).englishNumber().toString()}
+              </span>
+            </Col>
+          </Row>
+          <Row className="m-0 p-0 mt-3">
+            <Col xs={6} className="p-0 ps-4">
+              <Card className="border-0 bg-transparent text-light">
+                <Form.Label className="pe-3 fs-6">sip</Form.Label>
+                <Form.Control
+                  className="order-input py-2"
+                  type="number"
+                  name="voipNumber"
+                  defaultValue={props.employee?.voipNumber}
+                  onChange={handleChange}
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Card className="m-auto mt-3 productCard border-0 lh-lg pb-2">
+            <Card.Body className="pb-0 ps-0 emplyees-text-gray">
+              <Row className="m-0 p-0 d-flex align-items-center ">
+                <Col xs={12} className="p-0 ">
+                  <Card.Text className="employees--p--gray--desktop ">
+                    سطح دسترسی:
+                  </Card.Text>
+                </Col>
+                {props.show &&
+                  newPermission &&
+                  Object.keys(newPermission).map((key, index) => {
+                    if (key == "getDiscounts") return;
+                    return (
+                      <Col
+                        xs={4}
+                        className="p-0 pe-1 mt-2 employees-text-permission--desktop d-flex  align-items-center "
+                      >
+                        <label className={`mb-0 p-0 option--time--filter--groupe ${!newPermission[key] ? 'border-red' : null}`}>
+                          <input
+                            type="checkbox"
+                            name={key}
+                            id={`${index}`}
+                            onChange={handleChange}
+                            defaultChecked={newPermission[key]}
+                          />
+                          <span>
+                            <p className="p-0 m-0 fs-6">{translate(key)}</p>
+                          </span>
+                        </label>
+                      </Col>
+                    );
+                  })}
+              </Row>
+            </Card.Body>
+          </Card>
+          {editEmployeeLoading ? (
+            <Button
+              className="fw-bold order-submit border-0 w-100 mt-3"
+              size="lg"
+              type="submit"
+              disabled
+            >
+              <Spinner
+                as="span"
+                animation="grow"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              در حال انجام عملیات...
+            </Button>
+          ) : (
+            <Button
+              className="fw-bold btn-dark-blue notes-round border-0 w-100 mt-3"
+              size="lg"
+              type="submit"
+              block
+            >
+              ثبت
+            </Button>
+          )}
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
--- src/desktop/Components/employee/employees.js
+++ src/desktop/Components/employee/employees.js
@@ -1,125 +1,181 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Container, Card, Col, Spinner } from 'react-bootstrap'
-import persianJs from 'persianjs/persian.min';
-import { Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Container, Card, Col, Spinner } from "react-bootstrap";
+import persianJs from "persianjs/persian.min";
+import { makeStyles } from "@material-ui/core/styles";
 
 // Actions
-import { employeeActions } from '../../../actions/employeeActions'
+import { employeeActions } from "../../../actions/employeeActions";
 // Helpers
-import { translate } from '../../../helpers';
+import { translate } from "../../../helpers";
 
 // Components
-import { AddEmployee } from './addEmployee'
-import { EditEmployee } from './editEmployee'
-import { RemoveEmployee } from './removeEmployee'
-import { Applications } from './applications'
-import { Header } from '../base/header'
+import { AddEmployee } from "./addEmployee";
+import { EditEmployee } from "./editEmployee";
+import { RemoveEmployee } from "./removeEmployee";
+import { Header } from "../base/header";
 
 // Icons
-import editIcon from '../../assets/images/Products/edit.svg'
-import deleteIcon from '../../assets/images/discounts/deletee.svg'
-import checkIcon from '../../assets/images/tick.svg'
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-
+import editIcon from "../../assets/images/Products/edit.svg";
+import deleteIcon from "../../assets/images/discounts/deletee.svg";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        fontSize: "0.5rem",
-        color: "#4caf50"
-    }
+  root: {
+    fontSize: "0.5rem",
+    color: "#4caf50",
+  },
 }));
 
 export const Employees = () => {
+  const classes = useStyles();
+  const [addModalShow, setAddModalShow] = useState(false);
+  const [editModalShow, setEditModalShow] = useState(false);
+  const [removeModalShow, setRemoveModalShow] = useState(false);
+  const [employee, setEmployee] = useState({});
+  const sideBar = useSelector((state) => state.sideBar);
 
-    const classes = useStyles();
-    const [addModalShow, setAddModalShow] = useState(false)
-    const [editModalShow, setEditModalShow] = useState(false)
-    const [removeModalShow, setRemoveModalShow] = useState(false)
-    const [employee, setEmployee] = useState({})
-    const sideBar = useSelector(state => state.sideBar)
+  let employees = useSelector((state) => state.getEmployees.employees);
+  let getEmployeesLoading = useSelector((state) => state.getEmployees.loading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!addModalShow && !removeModalShow)
+      dispatch(employeeActions.getEmployees());
+  }, [dispatch, addModalShow, removeModalShow]);
 
-    let employees = useSelector(state => state.getEmployees.employees)
-    let getEmployeesLoading = useSelector(state => state.getEmployees.loading)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        if (!addModalShow && !removeModalShow)
-            dispatch(employeeActions.getEmployees())
-    }, [dispatch, addModalShow, removeModalShow])
+  return (
+    <>
+      <Header isBTNSearch={false} isBTNRequest={true} />
 
-    return (
-        <>
-            <Header isBTNSearch={false} isBTNRequest={true} />
-
-            <div className="product-page margin--top--header" style={{ paddingRight: sideBar.open ? "250px" : 0 }}>
-
-                <Container fluid className="m-0 w-100 d-flex justify-content-around flex-wrap ">
-
-                    {
-                        getEmployeesLoading &&
-                        <Col className="col-3 mt-2 m-auto d-block align-self-center w-100 mb-4 ">
-                            <Spinner className="m-auto d-block" animation="border" />
+      <div
+        className="product-page margin--top--header"
+        style={{ paddingRight: sideBar.open ? "250px" : 0 }}
+      >
+        <Container
+          fluid
+          className="m-0 d-flex justify-content-around flex-wrap"
+        >
+          {getEmployeesLoading && (
+            <Col className="col-3 mt-2 m-auto d-block align-self-center w-100 mb-4 ">
+              <Spinner className="m-auto d-block" animation="border" />
+            </Col>
+          )}
+          {employees
+            ? employees.map((item, index) => (
+                <Card
+                  key={index}
+                  className="m-0 p-0 mt-3 employees--card pt-3 px-3"
+                >
+                  <Card.Body className="p-0 rounded-3 d-flex flex-column justify-content-between">
+                    <Row className="m-0 p-0 ">
+                      <Card className="m-0 ms-3 mb-3 bg-light border-0 pb-2 flex-grow-0 radius-10">
+                        <Card.Body className="pb-0 ps-0 text-gray row radius-10">
+                          <Card.Text className="p-0  employees-text-gray--desktop col-4">
+                            <span>
+                              {item.family &&
+                                persianJs(item.family)
+                                  .englishNumber()
+                                  .toString()}
+                            </span>
+                          </Card.Text>
+                          <Card.Text className="p-0 employees-text-gray--desktop col-4">
+                            <span>
+                              {item.mobile &&
+                                persianJs(item.mobile)
+                                  .englishNumber()
+                                  .toString()}
+                            </span>
+                          </Card.Text>
+                          <Col className="m-0 mb-2 p-0 d-flex col-4">
+                            <Card.Text className="m-0 p-0 fs-6 ms-2 employees-text-gray--desktop">
+                              sip :
+                            </Card.Text>
+                            <Card.Text className="m-0 p-0 employees-text-gray--desktop">
+                              <span>
+                                {item.voipNumber &&
+                                  persianJs(item.voipNumber)
+                                    .englishNumber()
+                                    .toString()}
+                              </span>
+                            </Card.Text>
+                          </Col>
+                        </Card.Body>
+                      </Card>
+                      <Row className="m-0 p-0 d-flex align-items-center">
+                        <Col xs={6} className="p-0 ">
+                          <Card.Text className="employees-text-gray--desktop">
+                            سطح دسترسی:
+                          </Card.Text>
                         </Col>
-                    }
-                    {employees ?
-                        (employees.map((item, index) =>
-                            <Card key={index} className="m-0 p-0 mt-3 employees--card pt-3 px-3 mx-1" >
-                                <Card.Body className="p-0 rounded-3 d-flex flex-column justify-content-between">
-                                    <Row className="m-0 p-0 ">
-                                        <Card.Text className="p-0  employees-text-gray--desktop">
-                                            نام : <span>{item.family && persianJs(item.family).englishNumber().toString()}</span>
-                                        </Card.Text>
-                                        <Card.Text className="p-0 employees-text-gray--desktop">
-                                            موبایل : <span>{item.mobile && persianJs(item.mobile).englishNumber().toString()}</span>
-                                        </Card.Text>
-                                        <Col className="m-0 mb-2 p-0 d-flex  ">
-                                            <Card.Text className="m-0 p-0 fs-6 ms-2 employees-text-gray--desktop">
-                                                sip :
-                                            </Card.Text>
-                                            <Card.Text className="m-0 p-0 employees-text-gray--desktop">
-                                                <span>{item.voipNumber && persianJs(item.voipNumber).englishNumber().toString()}</span>
-                                            </Card.Text>
-                                        </Col>
-                                        
-                                        <Row className="m-0 p-0 d-flex align-items-center">
-                                            <Col xs={6} className="p-0 ">
-                                                <Card.Text className="employees-text-gray--desktop">
-                                                    سطح دسترسی:
-                                                </Card.Text>
-                                            </Col>
-                                            {
-                                                Object.keys(item.permission).map((per, index) =>
-                                                    per === "getDiscounts" ? null :
-                                                        item.permission[per]
-                                                            ? <Col xs={6} className="p-0 mt-2 employees-text-permission--desktop d-flex  align-items-center "><FiberManualRecordIcon classes={{ root: classes.root }} /><span className="me-1">{translate(per)}</span></Col>
-                                                            : null
-                                                )
-                                            }
-                                        </Row>
-                                    </Row>
-                                    <Row className="p-0 m-0 mt-4 d-flex justify-content-end" >
-                                        <Row className="justify-content-end">
-                                            <Card.Link className="d-flex justify-content-center editLogo m-0" onClick={() => { setRemoveModalShow(true); setEmployee(item) }}>
-                                                <img className="" src={deleteIcon} height="29px" alt="delete-icon" />
-                                            </Card.Link>
-                                            <Card.Link className="d-flex justify-content-center editLogo m-0" onClick={() => { setEditModalShow(true); setEmployee(item) }}>
-                                                <img className="" src={editIcon} height="39px" alt="edit-icon" />
-                                            </Card.Link>
-                                        </Row>
-                                    </Row>
-
-                                </Card.Body>
-                            </Card>
-                        ))
-
-                        : null
-                    }
-                </Container>
-                <AddEmployee show={addModalShow} onHide={() => setAddModalShow(false)} />
-                <EditEmployee show={editModalShow} onHide={() => setEditModalShow(false)} employee={employee} />
-                <RemoveEmployee show={removeModalShow} onHide={() => setRemoveModalShow(false)} employee={employee} />
-            </div >
-        </>
-    )
-}
+                        {Object.keys(item.permission).map((per, index) =>
+                          per === "getDiscounts" ? null : item.permission[
+                              per
+                            ] ? (
+                            <Col
+                              xs={6}
+                              className="p-0 mt-2 employees-text-permission--desktop d-flex  align-items-center "
+                            >
+                              <FiberManualRecordIcon
+                                classes={{ root: classes.root }}
+                              />
+                              <span className="me-1">{translate(per)}</span>
+                            </Col>
+                          ) : null
+                        )}
+                      </Row>
+                    </Row>
+                    <Row className="p-0 m-0 mt-4 d-flex justify-content-end">
+                      <Row className="justify-content-end">
+                        <Card.Link
+                          className="d-flex justify-content-center editLogo m-0"
+                          onClick={() => {
+                            setRemoveModalShow(true);
+                            setEmployee(item);
+                          }}
+                        >
+                          <img
+                            className=""
+                            src={deleteIcon}
+                            height="29px"
+                            alt="delete-icon"
+                          />
+                        </Card.Link>
+                        <Card.Link
+                          className="d-flex justify-content-center editLogo m-0"
+                          onClick={() => {
+                            setEditModalShow(true);
+                            setEmployee(item);
+                          }}
+                        >
+                          <img
+                            className=""
+                            src={editIcon}
+                            height="39px"
+                            alt="edit-icon"
+                          />
+                        </Card.Link>
+                      </Row>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              ))
+            : null}
+        </Container>
+        <AddEmployee
+          show={addModalShow}
+          onHide={() => setAddModalShow(false)}
+        />
+        <EditEmployee
+          show={editModalShow}
+          onHide={() => setEditModalShow(false)}
+          employee={employee}
+        />
+        <RemoveEmployee
+          show={removeModalShow}
+          onHide={() => setRemoveModalShow(false)}
+          employee={employee}
+        />
+      </div>
+    </>
+  );
+};
